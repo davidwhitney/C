aa560a57382710bc0aea68e4c369590f61b6ef01
David Whitney <david@davidwhitney.co.uk>
2018-02-25 16:34:52 +0000

    Added mime type.

---
+++ src/Easy/MimeType.js
@@ -0,0 +1,7 @@
+function solve() {
+    console.log("hello world");
+}
+
+module.exports = {
+    solve : solve
+}
---
+++ tests/Easy/MimeType.test.js
@@ -0,0 +1,6 @@
+require('../../src/CodingGame');
+const sut = require('../../src/Easy/MimeType');
+
+test("solve does example", function(){
+    sut.solve();
+});
