7847a0352214e8f5c71d6c62b7d42d59a69e5ecd
David Whitney <david@davidwhitney.co.uk>
2018-02-25 17:03:45 +0000

    MimeTypes solution.

--- src/Easy/MimeType.js
+++ src/Easy/MimeType.js
@@ -1,5 +1,26 @@
 function solve() {
-    console.log("hello world");
+
+    var N = parseInt(readline()); // Number of elements which make up the association table.
+    var Q = parseInt(readline()); // Number Q of file names to be analyzed.
+    
+    var types = [];
+    for (var i = 0; i < N; i++) {
+        var inputs = readline().split(' ');
+        types.push({k: inputs[0].toUpperCase(), v: inputs[1]});
+    }
+
+    for (var i = 0; i < Q; i++) {
+        var filename = readline();
+        if (filename.indexOf(".") == -1){
+            print("UNKNOWN");
+            continue;
+        }
+
+        var parts = filename.split(".");
+        var ext = parts[parts.length-1].toUpperCase();
+        var map = types.find(x => x.k == ext) || { k: "UNKNOWN", v: "UNKNOWN" }
+        print(map.v);
+    }
 }
 
 module.exports = {
--- tests/Easy/MimeType.test.js
+++ tests/Easy/MimeType.test.js
@@ -2,5 +2,12 @@ require('../../src/CodingGame');
 const sut = require('../../src/Easy/MimeType');
 
 test("solve does example", function(){
+    inputLines.push(2, 4, "html text/html", "png image/png", "test.html", "noextension", "portrait.png", "doc.TXT");
+
     sut.solve();
+
+    expect(outputLines[0]).toBe("text/html");
+    expect(outputLines[1]).toBe("UNKNOWN");
+    expect(outputLines[2]).toBe("image/png");
+    expect(outputLines[3]).toBe("UNKNOWN");
 });
