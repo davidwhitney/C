de0b895b066fc5a5ddb7249cea00774e59458795
David Whitney <david@davidwhitney.co.uk>
2018-02-26 00:19:02 +0000

    ahh tests share state, that sucks.

--- tests/Easy/ChuckNorris.test.js
+++ tests/Easy/ChuckNorris.test.js
@@ -5,12 +5,12 @@ test("solve returns correct value for sample", () => {
     inputLines.push("C");
     sut.solve();
 
-    expect(outputLines[0]).toBe("0 0 00 0000 0 00");
+    expect(outputLines).toContain("0 0 00 0000 0 00");
 });
 
 test("solve returns correct value for sample2", () => {
     inputLines.push("CC");
     sut.solve();   
 
-    expect(outputLines[0]).toBe("0 0 00 0000 0 000 00 0000 0 00");
+    expect(outputLines).toContain("0 0 00 0000 0 000 00 0000 0 00");
 });
